828b2b8d897903ac89f50bb8249ca329b41c871c
Anderson Richard da Silva <104325288+AndersonRichard@users.noreply.github.com>
2023-09-04 21:45:32 -0300

    Add files via upload

---
+++ GeradorEstatisticasFaltasCartoes.java
@@ -0,0 +1,17 @@
+package br.poo.geradorestatisticasbr;
+
+public class GeradorEstatisticasFaltasCartoes {
+
+	public EstatisticaFaltasCartoes calcularFaltasCartoes(Jogo jogo) {
+		return new EstatisticaFaltasCartoes(
+				jogo.getCartoesAmarelosVisitante(),
+				jogo.getCartoesVermelhosVisitante(),
+				jogo.getCartoesAmarelosMandante(),
+				jogo.getCartoesVermelhosMandante(),
+				jogo.getFaltasTimeVisitante(),
+				jogo.getFaltasTimeMandante()
+			);
+	}
+	
+
+}
---
+++ Imprimir.java
@@ -0,0 +1,7 @@
+package br.poo.geradorestatisticasbr;
+
+public interface Imprimir {
+    public interface Estatistica {
+        String imprimir(Jogo);
+    }
+}
